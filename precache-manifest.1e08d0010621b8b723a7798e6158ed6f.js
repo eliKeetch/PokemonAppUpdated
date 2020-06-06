b548829f0b5f3da6c210903e4cc2e77ceab9cdbe
Eli Keetch <elikeetch1996@gmail.com>
2020-06-06 12:22:38 -0700

    Updates

---
+++ precache-manifest.1e08d0010621b8b723a7798e6158ed6f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f774393c952b4c48db7706d534f7bf8c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6a25d3e74ece02713db",
+    "url": "/static/css/main.5e94140e.chunk.css"
+  },
+  {
+    "revision": "fb4429559097d97d41ee",
+    "url": "/static/js/2.693079a6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.693079a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6a25d3e74ece02713db",
+    "url": "/static/js/main.83df402b.chunk.js"
+  },
+  {
+    "revision": "119573916b10512504d3",
+    "url": "/static/js/runtime-main.dd81922d.js"
+  },
+  {
+    "revision": "edd72be48d41170cc2ee61a6c8f46657",
+    "url": "/static/media/newpokeball.edd72be4.png"
+  },
+  {
+    "revision": "de8a0f82f1449b48f71828ede116ecd9",
+    "url": "/static/media/newpokelogo.de8a0f82.png"
+  },
+  {
+    "revision": "d9e4253e086782d0b23cf6a7e4e69c7d",
+    "url": "/static/media/pokedex.d9e4253e.png"
+  }
+]);
